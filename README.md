4ebb892ca3a3d4faa718bf1b8d1bf167ef6f4e3b
Isael Cubas <isaelcubas@192.168.1.5>
2022-02-17 21:28:14 -0600

    first commit

--- README.md
+++ README.md
@@ -71,3 +71,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/t
 # emiluc
 # first_react_app
 # emiluc
+# emiluc
--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <h2>
+          Hola BICV;
+        </h2>
         <a
           className="App-link"
           href="https://reactjs.org"

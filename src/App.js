d3762f2b141bbe810e020b30daf3df1357cc1d32
Isael Cubas <isaelcubas@Isaels-MacBook-Air.local>
2022-02-19 18:08:54 -0600

    cambios190222

--- src/App.js
+++ src/App.js
@@ -6,30 +6,47 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
+        <h1>
+          Hola Brayan Cubas!!
+          <p>
+            Esto es React.
+          </p>
+        </h1>
         <h2>
-          Hola BICV;
+          ¿En que estado vives?
         </h2>
-        <h3>
-          Mis hijos se llaman;
-        </h3>
         <h4>
-          Emiliano y Luca;
+          Selecciona la opción correcta:
         </h4>
-
+        <div class="form-check form-switch">
+          <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+          <label class="form-check-label" for="flexSwitchCheckDefault">Oaxaca</label>
+        </div>
+          <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked"/>
+            <label class="form-check-label" for="flexSwitchCheckChecked">Ciudad de México</label>
+          </div>
+            <div class="form-check form-switch">
+              <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDisabled"/>
+              <label class="form-check-label" for="flexSwitchCheckDisabled">Veracruz</label>
+            </div>
+              <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckCheckedDisabled"/>
+                <label class="form-check-label" for="flexSwitchCheckCheckedDisabled">Guerrero</label>
+              </div>
+        <p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mr. Google
         </a>
+        </p>
       </header>
     </div>
+  
   );
 }
 
